a9c9a6a60da55f9f552a74d10357212a00bbc342
Ellie-exe <ryiscool247@gmail.com>
2022-10-28 12:54:07 -0400

    Added /all and /search endpoints

--- go.mod
+++ go.mod
@@ -5,6 +5,25 @@ go 1.19
 require (
 	github.com/JamesPEarly/loggly v0.0.0-20220929110912-1d839b0715fa // indirect
 	github.com/TwiN/go-color v1.2.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.17.0 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.17.9 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.22 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.10.1 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression v1.4.27 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.18 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.24 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.18 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.25 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.17.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.13.21 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.18 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.18 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.24 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.17.0 // indirect
+	github.com/aws/smithy-go v1.13.3 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,61 +1,196 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"github.com/JamesPEarly/loggly"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"net/http"
-	"time"
+	"regexp"
+	"sort"
 )
 
-type Time struct {
-	SystemTime string `json:"time"`
+type Post struct {
+	Title       string  `dynamodbav:"title"`
+	FullID      string  `dynamodbav:"name"`
+	Author      string  `dynamodbav:"author"`
+	Permalink   string  `dynamodbav:"permalink"`
+	URL         string  `dynamodbav:"url"`
+	DateCreated float64 `dynamodbav:"created_utc"`
 }
 
-type Log struct {
+type Status struct {
+	Table       string `json:"table"`
+	RecordCount int32  `json:"recordCount"`
+}
+
+type StatusLog struct {
 	Method      string `json:"method"`
 	SourceIP    string `json:"ip"`
 	RequestPath string `json:"path"`
 	StatusCode  int    `json:"status"`
 }
 
-func main() {
-	router := mux.NewRouter()
+var logger *loggly.ClientType
+var db *dynamodb.Client
+
+func init() {
 	_ = godotenv.Load()
 
-	router.HandleFunc("/rnelson3/status", handler)
-	router.PathPrefix("/rnelson3/").HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		res.WriteHeader(http.StatusNotFound)
-		log(req, http.StatusNotFound)
-	})
+	logger = loggly.New("rnelson3-server")
+	cfg, _ := config.LoadDefaultConfig(context.TODO())
+	db = dynamodb.NewFromConfig(cfg)
+}
 
-	_ = loggly.New("rnelson3-server").EchoSend("info", "Ready!")
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/rnelson3/all", allHandler).Methods("GET")
+	router.HandleFunc("/rnelson3/status", statusHandler).Methods("GET")
+	router.HandleFunc("/rnelson3/search", searchHandler).Methods("GET")
+	router.Methods("POST", "PUT", "PATCH", "DELETE").HandlerFunc(methodNotAllowedHandler)
+	router.PathPrefix("/rnelson3/").HandlerFunc(notFoundHandler)
+
+	_ = logger.EchoSend("info", "Ready!")
 	_ = http.ListenAndServe(":8080", router)
 }
 
-func handler(res http.ResponseWriter, req *http.Request) {
-	if req.Method != "GET" {
-		res.WriteHeader(http.StatusMethodNotAllowed)
-		log(req, http.StatusMethodNotAllowed)
-		return
+func allHandler(res http.ResponseWriter, req *http.Request) {
+	output, _ := db.Scan(context.TODO(), &dynamodb.ScanInput{TableName: aws.String("rnelson3-reddit")})
+
+	var posts []Post
+
+	for _, item := range output.Items {
+		var post Post
+		_ = attributevalue.UnmarshalMap(item, &post)
+		posts = append(posts, post)
 	}
+
+	sort.Slice(posts, func(i, j int) bool {
+		return posts[i].DateCreated > posts[j].DateCreated
+	})
+
+	data, _ := json.MarshalIndent(posts, "", "    ")
 
 	res.Header().Add("Content-Type", "application/json")
 	res.WriteHeader(http.StatusOK)
-
-	data, _ := json.MarshalIndent(Time{SystemTime: time.Now().String()}, "", "    ")
 	_, _ = res.Write(data)
-	log(req, http.StatusOK)
+
+	logRequest(req, http.StatusOK)
 }
 
-func log(req *http.Request, statusCode int) {
-	bytes, _ := json.MarshalIndent(Log{
+func statusHandler(res http.ResponseWriter, req *http.Request) {
+	output, _ := db.Scan(context.TODO(), &dynamodb.ScanInput{TableName: aws.String("rnelson3-reddit")})
+
+	status := Status{
+		Table:       "rnelson3-reddit",
+		RecordCount: output.Count,
+	}
+
+	data, _ := json.MarshalIndent(status, "", "    ")
+
+	res.Header().Add("Content-Type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	_, _ = res.Write(data)
+
+	logRequest(req, http.StatusOK)
+}
+
+func searchHandler(res http.ResponseWriter, req *http.Request) {
+	params := req.URL.Query()
+	var posts []Post
+
+	for key, value := range params {
+		match := true
+
+		switch key {
+		case "name":
+			match, _ = regexp.MatchString("t3_[a-z0-9]{6}", value[0])
+			break
+
+		case "author":
+			match, _ = regexp.MatchString("[A-Za-z0-9-_]+", value[0])
+			break
+
+		case "created_utc":
+			match, _ = regexp.MatchString("\\d{10}", value[0])
+			break
+
+		case "permalink":
+			match, _ = regexp.MatchString("/r/FloridaMan/comments/t3_[a-z0-9]{6}/.+", value[0])
+			break
+
+		case "title":
+			match, _ = regexp.MatchString(".+", value[0])
+			break
+
+		case "url":
+			match, _ = regexp.MatchString("https://.+", value[0])
+			break
+
+		default:
+			match = false
+			break
+		}
+
+		if !match {
+			res.WriteHeader(http.StatusBadRequest)
+			_, _ = res.Write([]byte("400 Bad Request"))
+			logRequest(req, http.StatusBadRequest)
+
+			return
+		}
+
+		filter := expression.Name(key).Equal(expression.Value(value[0]))
+		proj := expression.NamesList(expression.Name(key))
+		expr, _ := expression.NewBuilder().WithFilter(filter).WithProjection(proj).Build()
+
+		output, _ := db.Scan(context.TODO(), &dynamodb.ScanInput{
+			TableName:                 aws.String("rnelson3-reddit"),
+			ExpressionAttributeNames:  expr.Names(),
+			ExpressionAttributeValues: expr.Values(),
+			FilterExpression:          expr.Filter(),
+		})
+
+		_ = attributevalue.UnmarshalListOfMaps(output.Items, &posts)
+
+		break
+	}
+
+	data, _ := json.MarshalIndent(posts, "", "    ")
+
+	res.Header().Add("Content-Type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	_, _ = res.Write(data)
+
+	logRequest(req, http.StatusOK)
+}
+
+func methodNotAllowedHandler(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusMethodNotAllowed)
+	_, _ = res.Write([]byte("405 Method Not Allowed"))
+	logRequest(req, http.StatusMethodNotAllowed)
+}
+
+func notFoundHandler(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusNotFound)
+	_, _ = res.Write([]byte("404 Not Found"))
+	logRequest(req, http.StatusNotFound)
+}
+
+func logRequest(req *http.Request, statusCode int) {
+	bytes, _ := json.MarshalIndent(StatusLog{
 		Method:      req.Method,
 		SourceIP:    req.RemoteAddr,
 		RequestPath: req.URL.Path,
 		StatusCode:  statusCode,
 	}, "", "    ")
 
-	_ = loggly.New("rnelson3-server").Send("info", string(bytes))
+	_ = logger.EchoSend("info", string(bytes))
 }
